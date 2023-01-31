4e99d1eb07f82ce340d0c20c445541bdafe54998
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-01-31 19:10:21 +0000

    Controller show users final comments

--- controllers/showUsers.php
+++ controllers/showUsers.php
@@ -14,7 +14,7 @@
         $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
         $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
         
-        // DB query
+        // DB query to get users
         $sentencia = $conexion->prepare("SELECT * FROM User WHERE admin = 0");
         $sentencia->execute();
 
